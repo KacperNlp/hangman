db54889d680ee5ee67ed31a3632b427d25ce635b
Kacper Nalepa <kacper.nlp@wp.pl>
2020-11-29 20:04:23 +0100

    create class for password, winning and loosing functions, function to update entire panel

--- app/Game.js
+++ app/Game.js
@@ -1,16 +1,5 @@
 import Quote from './Quote.js';
 
-const allCategories = [{
-    category:"film",
-    passwords : ["Ogniem i mieczem", "Pan Tadeusz", "Gra o tron", "Potop", "Ironman"]
-},{
-    category:"książka",
-    passwords : ["Metro", "Polscy Bogowie wojny", "Lalka", "Janko Muzykant", "Lampo"]
-},{
-    category:"gra",
-    passwords : ["Cyberpunk", "Bannerlord", "Warband", "League of Legends", "CS GO"]
-}]
-
 class Game{
     constructor({lettersWrapper, categoryWrapper, wordWrapper, outputWrapper}){
         //all DOM elements 
@@ -27,36 +16,82 @@ class Game{
         this.password = new Quote(passwords);
     }
 
-    
+    //variables
+    //passwords with categories
     allCategories = [{
         category:"film",
-        passwords : ["Ogniem i mieczem", "Pan Tadeusz", "Gra o tron", "Potop", "Ironman"]
+        passwords : ["ogniem i mieczem", "pan tadeusz", "gra o tron", "potop", "ironman"]
     },{
         category:"książka",
-        passwords : ["Metro", "Polscy Bogowie wojny", "Lalka", "Janko Muzykant", "Lampo"]
+        passwords : ["metro", "polscy bogowie wojny", "lalka", "janko muzykant", "lampo"]
     },{
         category:"gra",
-        passwords : ["Cyberpunk", "Bannerlord", "Warband", "League of Legends", "CS GO"]
+        passwords : ["cyberpunk", "bannerlord", "warband", "league of legends", "cs go"]
     }];
 
-    startGame(){
+    //player lives
+    currentLifes = 0;
+    allLifes = 7;
 
+    //methods/functions
+    drawLetters(){
         //loop for add letters in game keyboard
         for(let i = 0; i<26; i++){
             const letter = (i + 10).toString(36);
             const btn = document.createElement('button'); //everyone letter has button
             btn.innerHTML = letter; //add letter to btn
             this.lettersWrapper.appendChild(btn); //add btn to letters wrapper (DOM element)
-            btn.addEventListener('click', ()=>guess(letter)) //everyone btn with letter has guessing event that checks if the letters is in the password  
+            btn.addEventListener('click', (e)=>this.guess(e,letter)) //everyone btn with letter has guessing event that checks if the letters is in the password  
         }
+    }
+    //draw quote/password
+    drawQuote(){
+        const hidePassword = this.password.hidePassword();
+        this.wordWrapper.innerHTML = hidePassword;
+    }
+
+    startGame(){
+        document.getElementsByClassName('step')[this.currentLifes].style.opacity = 1;
+        //this function is drawing letters
+        this.drawLetters();
+        //draw quote/password 
+        this.drawQuote();
     }
 
     //this function checks if the letters is in the password, function for letter btn event 
-    guess(letter){
-        console.log('letter');
+    guess(e,letter){
+        e.target.disabled = true;
+        //checks that the password include a letter
+        const isGoodAnswer = this.password.checkLetter(letter);
+        if(!isGoodAnswer){
+            this.updateLifes()
+            //if user has lost all lives
+            if(this.currentLifes == this.allLifes) this.loosing()
+            return
+        };
+        //every time when user choose letter, this function will draw agian password/quote 
+        this.drawQuote();
+        if(!this.password.hidePassword().includes('_')) return this.winning();
+    }
+
+    updateLifes(){
+        //incremrent current life
+        this.currentLifes++;
+
+        //change style of img of new current life
+        document.getElementsByClassName('step')[this.currentLifes].style.opacity = 1;
     }
 
 
+    //winning and loosing functions
+    winning(){
+        this.wordWrapper.innerHTML = "Wygrałeś!";
+        this.lettersWrapper.innerHTML = '';
+    }
+    loosing(){
+        this.wordWrapper.innerHTML = "Przegrałeś!";
+        this.lettersWrapper.innerHTML = '';
+    }
 }
 
 export default Game;
--- app/Quote.js
+++ app/Quote.js
@@ -1,6 +1,27 @@
 class Quote{
     constructor(txt){
-        this.txt = txt;
+        this.password = txt[Math.floor(Math.random() * txt.length)];
+
+        this.guessed = [];
+    }
+
+    hidePassword(){
+        let content = '';
+        for(const char of this.password){
+            if(char == ' ' || this.guessed.includes(char)){
+                content += char;
+            }else{
+                content+= '_'
+            }
+        }
+        return content;
+    }
+
+    checkLetter(letter){
+        if(!this.password.includes(letter)) return false;
+
+        this.guessed.push(letter);
+        return true;
     }
 }
 
