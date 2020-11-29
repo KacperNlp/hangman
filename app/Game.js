5d448f823f85b4bedf3abf2379303bd9125d05c0
Kacper Nalepa <kacper.nlp@wp.pl>
2020-11-29 16:03:42 +0100

    create startGame function in Game and loop of the letters in this function

--- app/Game.js
+++ app/Game.js
@@ -7,4 +7,18 @@ class Game{
         this.wordWrapper = wordWrapper;
         this.outputWrapper = outputWrapper;
     }
+
+    startGame(){
+
+        //loop for add letters in game keyboard
+        for(let i = 0; i<=26; i++){
+            const letter = (i + 10).toString(36);
+            const btn = document.createElement('button'); //everyone letter has button
+            btn.innerHTML = btn; //add letter to btn
+            this.lettersWrapper.appendChild(btn); //add btn to letters wrapper (DOM element)
+            btn.addEventListener('click', ()=>guess(letter)) //everyone btn with letter has guessing event that checks if the letters is in the password  
+        }
+    }
+
+
 }
